46dc3677654051589e413ef9deedc767341fbf76
Zhaoqi Tong <zhaoqi.tong@outlook.com>
2023-11-01 09:42:07 +0800

    link scroll

--- app/components/UrlLink.tsx
+++ app/components/UrlLink.tsx
@@ -11,7 +11,7 @@ export default function UrlLink({url, bold = true, underline = true, fontSize = 
     return (
         <Link
             className={`${fontSize} ${bold ? 'font-bold' : ''} ${underline ? 'underline underline-offset-[5px] decoration-black/60 decoration-1' : ''} text-black hover:text-orange-400 duration-300`}
-            href={url} scroll={false}>
+            href={url}>
             {children}
         </Link>
     )
